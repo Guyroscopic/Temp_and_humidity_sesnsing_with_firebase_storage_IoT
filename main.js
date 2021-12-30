1bb1e08d1bf363a2692f18faa2fc350e9c852dd3
ahsan-tikito <ahsan@tikito.app>
2021-12-30 23:54:02 +0500

    finalized minor

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ const firebaseConfig = {
   };
 const app = firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-database.ref('Humidity').on('value',(snapshot)=>{
+database.ref('Humidity').once('value',(snapshot)=>{
+    document.getElementById("allGraphs").style.display = "block";
+    document.getElementById("loader").style.display = "none";
     fetchedData = Object.values(snapshot.val());
     console.log(fetchedData);
     lineChart(fetchedData, 'humidityChart', "Humidity");
     barChart(fetchedData, 'humidityBarChart', "Humidity");
 })
-database.ref('Temperature').on('value',(snapshot)=>{
+database.ref('Temperature').once('value',(snapshot)=>{
     fetchedData = Object.values(snapshot.val());
     console.log(fetchedData);
     lineChart(fetchedData, 'temperatureChart', 'Temperature');
@@ -26,7 +28,7 @@ function lineChart(fetchedData, id,label) {
         const myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+                labels: Array.from(Array(Object.keys(fetchedData).length).keys()),
                 datasets: [{
                     label: label,
                     data: fetchedData,
@@ -55,9 +57,9 @@ function lineChart(fetchedData, id,label) {
 function barChart(fetchedData, id,label) {
     const ctx = document.getElementById(id).getContext('2d');
     const myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'scatter',
         data: {
-            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+            labels: Array.from(Array(Object.keys(fetchedData).length).keys()),
             datasets: [{
                 label: label,
                 data: fetchedData,
